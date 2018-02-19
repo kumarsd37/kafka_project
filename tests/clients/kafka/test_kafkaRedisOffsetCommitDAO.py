18ffa436301b3f4f41db724f7a7b251485835de0
Pavan Tummalapalli <pavan.tummalapalli@revionics.com>
2018-02-19 18:58:57 +0530

    modified test cases to support consumer group

--- tests/clients/kafka/test_kafkaRedisOffsetCommitDAO.py
+++ tests/clients/kafka/test_kafkaRedisOffsetCommitDAO.py
@@ -21,21 +21,25 @@ class TestKafkaRedisOffsetCommitDAO(TestCase):
         except Exception as exc:
             raise exc
 
-        self.kafka_redis_offset_commit_dao = KafkaRedisOffsetCommitDAO(self.redis_connection, namespace='kafka', delimiter=':')
+        consumer_group = 'TESTING_BLOB_CG'
+        namespace = 'testing'
+        delimiter = ':'
+
+        self.kafka_redis_offset_commit_dao = KafkaRedisOffsetCommitDAO(self.redis_connection, consumer_group=consumer_group, namespace=namespace, delimiter=delimiter)
 
     def tearDown(self):
         del self.kafka_redis_offset_commit_dao
         del self.redis_connection
 
     def test_commit_offset(self):
-        tpo = TopicPartitionOffset('ARTICLE', 0, 0)
+        tpo = TopicPartitionOffset('TESTING', 0, 0)
         response = self.kafka_redis_offset_commit_dao.commit_offset(tpo)
         self.assertTrue(response)
 
     def test_get_all_topics_partitions_offset(self):
-        tpo0 = TopicPartitionOffset('ARTICLE', 0, 0)
-        tpo1 = TopicPartitionOffset('ARTICLE', 1, 0)
-        tpo2 = TopicPartitionOffset('ARTICLE', 2, 0)
+        tpo0 = TopicPartitionOffset('TESTING', 0, 0)
+        tpo1 = TopicPartitionOffset('TESTING', 1, 0)
+        tpo2 = TopicPartitionOffset('TESTING', 2, 0)
         for i in range(0, 3):
             obj = eval('tpo'+str(i))
             response = self.kafka_redis_offset_commit_dao.commit_offset(obj)
@@ -44,19 +48,19 @@ class TestKafkaRedisOffsetCommitDAO(TestCase):
         self.assertGreaterEqual(len(topic_partition_offset_list), 3)
 
     def test_get_topic_all_partitions_offset(self):
-        tpo0 = TopicPartitionOffset('ARTICLE', 0, 0)
-        tpo1 = TopicPartitionOffset('ARTICLE', 1, 0)
-        tpo2 = TopicPartitionOffset('ARTICLE', 2, 0)
+        tpo0 = TopicPartitionOffset('TESTING', 0, 0)
+        tpo1 = TopicPartitionOffset('TESTING', 1, 0)
+        tpo2 = TopicPartitionOffset('TESTING', 2, 0)
         for i in range(0, 3):
             obj = eval('tpo' + str(i))
             response = self.kafka_redis_offset_commit_dao.commit_offset(obj)
             self.assertTrue(response)
-        topic = 'ARTICLE'
+        topic = 'TESTING'
         topic_partition_offset_list = self.kafka_redis_offset_commit_dao.get_topic_all_partitions_offset(topic)
         self.assertGreaterEqual(len(topic_partition_offset_list), 3)
 
     def test_get_topic_partition_offset(self):
-        tpo0 = TopicPartitionOffset('ARTICLE', 0, 0)
+        tpo0 = TopicPartitionOffset('TESTING', 0, 0)
         response = self.kafka_redis_offset_commit_dao.commit_offset(tpo0)
         self.assertTrue(response)
         topic_partition_offset = self.kafka_redis_offset_commit_dao.get_topic_partition_offset(tpo0)
